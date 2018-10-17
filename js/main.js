27ac6b805eea58506ffd87aae13f8492faa3ae47
Enrique Martel <enrique@meta.edu>
2018-10-16 21:34:28 -0300

    funcional

--- js/main.js
+++ js/main.js
@@ -2,18 +2,45 @@ var n1 = document.querySelector('#n1')
 var n2 = document.querySelector('#n2')
 var resulta = document.querySelector('#result')
 
-function somar() {
-    return resultado(parseInt(n1.value) + parseInt(n2.value))
-}
-function subtrair() {
-    return resultado(parseInt(n1.value) - parseInt(n2.value))
-}
-function multiplicar() {
-    return resultado(parseInt(n1.value) * parseInt(n2.value))
-}
-function dividir() {
+document.querySelectorAll('input').forEach(function (input_html) {
+    console.log(input_html)
+    input_html.addEventListener('change', function (ev) {
+        var en = parseInt(ev.target.value)
+        if (isNaN(en)||!isFinite(en)){
+             onunload.apply()
+    }})
+})
+
+document.querySelector("#dividir").addEventListener('mousemove', function () {
     resultado(parseInt(n1.value) / parseInt(n2.value))
-}
+})
+document.querySelector("#mult").addEventListener('mousemove', function () {
+    resultado(parseInt(n1.value) * parseInt(n2.value))
+})
+document.querySelector("#somar").addEventListener('mousemove', function () {
+    resultado(parseInt(n1.value) + parseInt(n2.value))
+})
+document.querySelector("#sub").addEventListener('mousemove', function () {
+    resultado(parseInt(n1.value) - parseInt(n2.value))
+})
+document.querySelector("#limpar").addEventListener('dblclick', function () {
+    n1.textContent = ''
+    n2.textContent = ''
+    resulta.textContent = '0'
+})
+
+// function somar() {
+//     return resultado(parseInt(n1.value) + parseInt(n2.value))
+// }
+// function subtrair() {
+//     return resultado(parseInt(n1.value) - parseInt(n2.value))
+// }
+// function multiplicar() {
+//     return resultado()
+// }
+// function dividir() {
+//     resultado(parseInt(n1.value) / parseInt(n2.value))
+// }
 function resultado(result) {
     if (valideNaN(result)) {
         if (!valideFinite(result)){
